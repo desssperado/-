caac7edae96ce7f230d81b40c3a29ff0056ec47d
desssperado <gamzatgasanov0@gmail.com>
2024-10-13 03:40:21 +0300

    Update modul_3_5.py

--- modul_3_5.py
+++ modul_3_5.py
@@ -4,9 +4,9 @@ def get_multiplied_digits(number):
         first = int(str_number[0])
         if first == 0:
             return first * get_multiplied_digits(int(str_number[1:]))
-        return first * get_multiplied_digits(int(str_numder[1:]))
+        return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return int(str_number) if int(str_number) != 0 else 1
-numder = 3455300
+number = 23412300
 result = get_multiplied_digits(number)
 print(result)
